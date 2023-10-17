7c5d70962cd085269935b80f12f0bb61e4924f14
Richard Raya <rdxzv.dev@gmail.com>
2023-10-17 19:53:04 -0300

    Revert "ParanoidSystemUI: Implement Bluetooth dialog"
    This reverts commit 715fa3012f237fce93931f6c079219f772f417b9.
    
    Change-Id: I21aa4d4cf85aa98750f93cfb7583fd80bdbf083c

--- src/co/aospa/systemui/qs/tileimpl/ParanoidQSModule.kt
+++ src/co/aospa/systemui/qs/tileimpl/ParanoidQSModule.kt
@@ -19,7 +19,6 @@ package co.aospa.systemui.qs.tileimpl;
 import com.android.systemui.qs.tileimpl.QSTileImpl
 
 import co.aospa.systemui.qs.tiles.AlwaysOnDisplayTile;
-import co.aospa.systemui.qs.tiles.BluetoothDialogTile;
 import co.aospa.systemui.qs.tiles.CaffeineTile;
 import co.aospa.systemui.qs.tiles.CellularTile
 import co.aospa.systemui.qs.tiles.DataSwitchTile;
@@ -40,12 +39,6 @@ interface ParanoidQSModule {
     @IntoMap
     @StringKey(AlwaysOnDisplayTile.TILE_SPEC)
     fun bindAlwaysOnDisplayTile(alwaysOnDisplayTile: AlwaysOnDisplayTile): QSTileImpl<*>
-
-    /** Inject BluetoothDialogTile into tileMap in QSModule */
-    @Binds
-    @IntoMap
-    @StringKey(BluetoothDialogTile.TILE_SPEC)
-    fun bindBluetoothDialogTile(bluetoothDialogTile: BluetoothDialogTile): QSTileImpl<*>
 
     /** Inject CaffeineTile into tileMap in QSModule */
     @Binds
